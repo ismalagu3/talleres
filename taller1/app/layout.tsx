79520c763b882be3d32f6acb0c05302240dca75a
ismalagu3 <ismalagu3@gmail.com>
2025-04-01 19:48:06 +0200

    usamos métricas

--- taller1/app/layout.tsx
+++ taller1/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 
+Analytics;
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
